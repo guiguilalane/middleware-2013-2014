4ae4b909e5bfc923c0969e412148eafa27e96873
guiguilalane <poissondu44@hotmail.fr>
2013-12-02 18:36:08 +0100

    rebase

--- api/src/main/java/fr/alma/middleware1314/api/share/Share.java
+++
@@ -1,9 +0,0 @@
-package main.java.fr.alma.middleware1314.api.share;
-
-import main.java.fr.alma.middleware1314.api.aggregation.Aggregation;
-
-public interface Share {
-	
-	public void share( Aggregation aggregation, Visibility visibility );
-	
-}
--- api/src/main/java/fr/alma/middleware1314/api/share/Visibility.java
+++
@@ -1,7 +0,0 @@
-package main.java.fr.alma.middleware1314.api.share;
-
-public enum Visibility {
-	
-	PRIVATE, PUBLIC // , FRIENDS 
-	
-}
